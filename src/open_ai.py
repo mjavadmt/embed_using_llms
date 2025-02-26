6d4e40c06b18d93eb46583fc417718f125165673
Mootez Saad <mootez@dal.ca>
2025-02-26 15:19:56 -0400

    chore: use text-embedding-3-small model in OpenAI's method

--- src/open_ai.py
+++ src/open_ai.py
@@ -1,18 +1,16 @@
 import os
 import openai
-from dotenv import load_dotenv
 
-load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 
-def get_code_embedding(code_snippet):
+def get_openai_code_embedding(code_snippet):
     """
     Get embeddings for code snippets using OpenAI's API.
     """
     try:
         response = openai.embeddings.create(
-            model="text-embedding-3-large",
+            model="text-embedding-3-small",
             input=code_snippet,
             encoding_format="float",
             dimensions=3072  # Optional: maximum dimensions for more detailed code representation
@@ -23,13 +21,4 @@ def get_code_embedding(code_snippet):
     except Exception as e:
         print(f"Error getting embedding: {e}")
         return None
-
-
-if __name__ == "__main__":
-    sample_code = "def hello_world():\n    print('Namaste!')"
-
-    embedding = get_code_embedding(sample_code)
-    if embedding:
-        print(f"Embedding dimension: {len(embedding)}")
-        print(f"First few values: {embedding[:5]}")
 
